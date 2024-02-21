ea0d550ef1271a867dd227734efd69edad3254c2
TheLurch27 <kevinnituom1996.sae@t-online.de>
2024-02-21 22:43:56 +0100

    Added Player Movement & Butler Movement

--- CrazyCrown_Test/Assets/Prog/Scripts/ButlerController.cs
+++ CrazyCrown_Test/Assets/Prog/Scripts/ButlerController.cs
@@ -1,18 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ButlerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float movementSpeed = 2f;
+    public float stoppingDistance = 2f;
+
+    private Transform playerTransform;
+    private bool isMoving = false;
+
+    private void Start()
     {
-        
+        // Den Spieler finden
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            playerTransform = player.transform;
+
+            // Bewegung starten
+            StartMoving();
+        }
+        else
+        {
+            Debug.LogError("Player not found!");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isMoving && playerTransform != null)
+        {
+            // Berechne die Richtung zum Spieler
+            Vector3 direction = playerTransform.position - transform.position;
+            direction.z = 0f; // Stellen Sie sicher, dass die Bewegung nur in der Ebene bleibt
+
+            // Überprüfe, ob der Butler die gewünschte Entfernung zum Spieler erreicht hat
+            if (direction.magnitude > stoppingDistance)
+            {
+                // Bewege den Butler in Richtung des Spielers
+                transform.Translate(direction.normalized * movementSpeed * Time.deltaTime);
+            }
+            else
+            {
+                // Stoppe die Bewegung, wenn die gewünschte Entfernung erreicht ist
+                StopMoving();
+            }
+        }
+    }
+
+    private void StartMoving()
+    {
+        isMoving = true;
+    }
+
+    private void StopMoving()
+    {
+        isMoving = false;
     }
 }
--- CrazyCrown_Test/Assets/Prog/Scripts/PlayerController.cs
+++ CrazyCrown_Test/Assets/Prog/Scripts/PlayerController.cs
@@ -1,18 +1,94 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private InputSystem inputSystem;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool isSaluting = false;
+
+    public float moveSpeed = 5f;
+
+    private void Awake()
     {
-        
+        inputSystem = new InputSystem();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        inputSystem.Enable();
+        inputSystem.Movement.Walk.performed += ctx => MoveCharacter(ctx.ReadValue<Vector2>());
+        inputSystem.Movement.Walk.canceled += ctx => MoveCharacter(Vector2.zero);
+        inputSystem.Salute.Salute.started += ctx => StartSalute();
+        inputSystem.Salute.Salute.canceled += ctx => EndSalute();
+    }
+
+    private void OnDisable()
+    {
+        inputSystem.Disable();
+        inputSystem.Movement.Walk.performed -= ctx => MoveCharacter(ctx.ReadValue<Vector2>());
+        inputSystem.Movement.Walk.canceled -= ctx => MoveCharacter(Vector2.zero);
+        inputSystem.Salute.Salute.started -= ctx => StartSalute();
+        inputSystem.Salute.Salute.canceled -= ctx => EndSalute();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isSaluting)
+        {
+            MoveCharacter(inputSystem.Movement.Walk.ReadValue<Vector2>());
+        }
+    }
+
+    private void MoveCharacter(Vector2 direction)
+    {
+        if (!isSaluting)
+        {
+            rb.velocity = direction * moveSpeed;
+            UpdateAnimation(direction);
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void StartSalute()
+    {
+        isSaluting = true;
+        rb.velocity = Vector2.zero;
+        animator.SetBool("isSaluting", true);
+        animator.SetBool("isWalking", false);
+    }
+
+    private void EndSalute()
+    {
+        isSaluting = false;
+        animator.SetBool("isSaluting", false);
+    }
+
+    private void UpdateAnimation(Vector2 direction)
+    {
+        if (direction.magnitude > 0)
+        {
+            animator.SetBool("isWalking", true);
+            if (direction.x < 0) // Wenn die Richtung nach links zeigt
+            {
+                // Das Sprite spiegeln, wenn nach links gelaufen wird
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else if (direction.x > 0) // Wenn die Richtung nach rechts zeigt
+            {
+                // Das Sprite wieder in die Ausgangsrichtung bringen
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
     }
 }
